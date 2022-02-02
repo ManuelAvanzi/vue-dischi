c6a6eefda2bf19bc55c262bf8a0b4a276c9a2604
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-02 18:50:14 +0100

    loader e setTimeout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <header-container />
-    <album-container :albumList="albumList"/>
+    <loader-container v-if="!flagLoaded" />
+    <header-container v-else/>
+    <album-container  :albumList="albumList"/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import LoaderContainer from './components/LoaderContainer.vue'
 import AlbumContainer from './components/AlbumContainer.vue'
 import HeaderContainer from './components/HeaderContainer.vue'
 
@@ -17,18 +19,25 @@ export default {
 
   components: {
     HeaderContainer,
-    AlbumContainer
+    AlbumContainer,
+    LoaderContainer
   },
      
      data(){
        return {
-         albumList:[]
+         albumList:[],
+         flagLoaded:false
        }
      },
   mounted() {
+
+    setTimeout( () => {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response)=>{
-      this.albumList=response.data.response
+     
+     this.albumList=response.data.response
+     this.flagLoaded=true;
     })
+    }, 1000)
   }
 }
 </script>
